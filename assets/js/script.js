7c75255e113477e68157bd7acadb37b613c963a5
Rommy Gustiawan <rgcppp@gmail.com>
2021-01-03 17:15:42 +0700

    Mengisi kerangka halaman index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 const getCurrentYear = () => {
   return new Date().getFullYear();
 };
-document.getElementById('current_year').innerHTML = getCurrentYear();
+const currentYearElement = document.getElementById('current_year');
+if (currentYearElement) {
+  currentYearElement.innerHTML = getCurrentYear();
+}
+
+const getGreets = () => {
+  const hour = new Date().getHours();
+  if (hour < 12) {
+    return 'Morning';
+  } else if (hour >= 12 && hour <= 16) {
+    return 'Afternoon';
+  } else if (hour > 16 && hour < 19) {
+    return 'Evening';
+  } else {
+    return 'Night';
+  }
+};
+const greetsElement = document.getElementById('greets');
+if (greetsElement) {
+  greetsElement.innerHTML = getGreets();
+}
